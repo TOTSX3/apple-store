2c6d77b52982cb29500ea093b5182a4f04b1dfe9
Ammar Ahmed <131693658+TOTSX3@users.noreply.github.com>
2023-04-25 16:15:53 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+// let Certificate = document.getElementById("Certificate");
+// console.log(Certificate);
+
+// let Cer = document.getElementById("Cer");
+// console.log(Cer);
+
+// Certificate.addEventListener("click", function () {
+
+//   Cer.classList.toggle("hideng")
+
+
+  
+//   Certificate.classList.toggle("bordar")
+
+
+
+
+// })
